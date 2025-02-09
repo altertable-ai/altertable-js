c4a8870c5d5255be1e6f1e8876502d80549b4d06
Sylvain Utard <sylvain.utard@gmail.com>
2025-02-09 12:47:26 +0100

    Monorepo architecture

---
+++ packages/analytics.js/src/index.ts
@@ -0,0 +1,6 @@
+export const Reaping = () => {
+  console.log('Reaping');
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(global as any).Reaping = Reaping;
---
+++ packages/analytics.js/test/index.test.tsx
@@ -0,0 +1,6 @@
+import { expect, test } from 'vitest';
+import { Reaping } from '../src';
+
+test('index', () => {
+  expect(Reaping).toBeDefined();
+});
