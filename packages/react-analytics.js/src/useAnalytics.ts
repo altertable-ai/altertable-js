54b8833ea50601e0c4585c6d065ad5c7fc74041c
Sylvain Utard <sylvain.utard@gmail.com>
2025-02-19 22:22:23 +0100

    Plug React hook with analytics.js

--- packages/react-analytics.js/src/useAnalytics.ts
+++ packages/react-analytics.js/src/useAnalytics.ts
@@ -1,20 +1,27 @@
 import { useCallback } from 'react';
 import { FunnelMapping, FunnelStepNames, FunnelStepProperties } from './types';
+import { reaping } from '@reaping-ai/analytics.js';
+import type { Reaping } from '@reaping-ai/analytics.js';
 
 export const useAnalytics = <T extends FunnelMapping>() => {
+  const instance = (window.Reaping || reaping) as Reaping;
+
   const makeTrack = <Steps extends T[keyof T] = T[keyof T]>() => {
     return <Step extends FunnelStepNames<Steps>>(
       step: Step,
       properties: FunnelStepProperties<Steps, Step>
     ) => {
-      // Replace this with your actual analytics tracking logic.
-      console.log(`Tracking step: ${step}`, properties);
+      try {
+        instance.track(step, properties);
+      } catch (error) {
+        console.error('Failed to track event', error);
+      }
     };
   };
 
   const useFunnel = useCallback(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    <FunnelName extends keyof T>(funnelName: FunnelName) => {
+    <FunnelName extends keyof T>(_funnelName: FunnelName) => {
       const track = useCallback(makeTrack<T[FunnelName]>(), []);
       return { track };
     },
@@ -23,8 +30,17 @@ export const useAnalytics = <T extends FunnelMapping>() => {
 
   const track = useCallback(makeTrack(), []);
 
+  const identify = useCallback((userId: string) => {
+    try {
+      instance.identify(userId);
+    } catch (error) {
+      console.error('Failed to identify user', error);
+    }
+  }, []);
+
   return {
     useFunnel,
+    identify,
     track,
   };
 };
