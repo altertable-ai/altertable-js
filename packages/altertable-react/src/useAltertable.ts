a5c3417d9820a34726f2afc45ab5b2c61972ff72
François Chalifour <francoischalifour@users.noreply.github.com>
2025-12-01 11:46:23 +0100

    Expose alias API in React package

--- packages/altertable-react/src/useAltertable.ts
+++ packages/altertable-react/src/useAltertable.ts
@@ -87,6 +87,7 @@ export function useAltertable<T extends FunnelMapping>() {
 
   return useMemo(
     () => ({
+      alias: altertable.alias.bind(altertable),
       configure: altertable.configure.bind(altertable),
       getTrackingConsent: altertable.getTrackingConsent.bind(altertable),
       identify: altertable.identify.bind(altertable),
